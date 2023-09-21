625afc265c24ee0af08074db56f663398677a3f7
goott3 <AlEridaniB@gmail.com>
2023-09-21 16:55:56 +0900

    장바구니 넣어두기

--- Programers_CodingTest/src/algo/test13.java
+++ Programers_CodingTest/src/algo/test13.java
@@ -3,24 +3,39 @@ package algo;
 public class test13 {
 
 	public static void main(String[] args) {
-		int[] people = {70, 50, 80, 50};
-		int limit = 100;
-		System.out.println(solution(people, limit));
+		int[] people = {4, 3, 2, 1};
+		
+		for(Integer x : solution(people))
+		System.out.println(x);
 	}
 	
+	 public static int[] solution(int[] arr) {
+	   int[] answer = new int[arr.length-1];
+       if(arr.length == 1){
+           int[] x = {-1};
+           return x;
+       }
+       int min = Integer.MAX_VALUE;
+       for(Integer x : arr) {
+    	   if(x < min) {
+    		min = x;   
+    	   }   
+       }
+       for(int i =0; i < arr.length;i++) {
+    	   if(arr[i] == min) {
+    		   arr[i] = 0;
+    	   }
+       }
+       int count = 0;
+       for(int i = 0; i < arr.length; i ++) {
+    	   if(arr[i] != 0) {
+    		   answer[count] = arr[i];
+    		   count++;
+    	   }
+       }
+       
+     
+       return answer;
 	
-	    public static int solution(int[] people, int limit) {
-	        int answer = 0;
-	        int size = people.length;
-	        int weitght = 0;
-	        for(int i =0; i < size;i++) {
-	        	if(!(i + 1 > size)) {
-	        		if(weitght > people[i] + people[i+1])
-	        		weitght = people[i] + people[i+1];
-	        	}
-	        }
-	        return answer;
-	    }
-	
-
+	 }
 }
---
+++ Programers_CodingTest/src/algo/test15.java
@@ -0,0 +1,24 @@
+package algo;
+
+public class test15 {
+
+	public static void main(String[] args) {
+		String str = "3people unFollowed me";
+		System.out.println(solution(str));
+
+	}
+	
+	public static String solution(String s) {
+        String answer = "";
+        String[] arrStr = s.split(" ");
+        
+        for(int i =0; i < arrStr.length;i++) {
+        	char ch = arrStr[i].charAt(0);
+        	if(ch >= 'a' && ch <='z') {
+        		arrStr[i].
+        	}
+        }
+        return answer;
+    }
+
+}
